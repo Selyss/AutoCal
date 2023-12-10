703076ee681b4ce2af510060df8fa1fcc0365a16
Selyss <99344963+Selyss@users.noreply.github.com>
2023-12-09 22:33:40 -0500

    REPL

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/user"
+
+	"github.com/Selyss/AutoCal/repl"
 )
 
 func main() {
@@ -12,4 +15,6 @@ func main() {
 	}
 	fmt.Printf("Hello %s! This is AutoCal, your smart calendar automation tool.\n", // TODO: add cobra
 		user.Username)
+	fmt.Printf("Start by typing in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
