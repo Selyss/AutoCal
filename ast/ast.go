b95e3aee8b4de02b70860d90653ad0b2f3753b6a
Selyss <99344963+Selyss@users.noreply.github.com>
2023-12-11 17:21:11 -0500

    start of ast

---
+++ ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "github.com/Selyss/AutoCal/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type AddStatement struct {
+	Value Expression
+	Name  *Identifier
+	Token token.Token // the token.ADD token
+}
+
+func (ls *AddStatement) statementNode()       {}
+func (ls *AddStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
